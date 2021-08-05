b034aeb68d9be4e3b89a0097f5f8b0dc8879c7a5
sammyhy <sammykkoon@gmail.com>
2021-08-05 17:58:34 +0900

    learning~~

---
+++ schemas/index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+  if (process.env.NODE_ENV !== "production") {
+    mongoose.set("debug", true);
+  }
+  mongoose.connect(
+    "mongodb+srv://sammy:rjseka12@cluster0.pmeti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      dbName: "nodejs",
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    },
+    (error) => {
+      if (error) {
+        console.log("connection error", error);
+      } else {
+        console.log("connection success");
+      }
+    }
+  );
+};
+
+mongoose.connection.on("error", (error) => {
+  console.error("connection error", error);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.error("disconnected, reconnecting~~");
+  connect();
+});
+
+module.exports = connect;
